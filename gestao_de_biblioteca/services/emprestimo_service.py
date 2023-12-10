0317eeeeed6a3997263a77c02a6ca67de0d45e41
natalia-hnzn <natalia.hnzn@gmail.com>
2023-12-10 17:46:41 -0300

    recupera emprestimo fix

--- gestao_de_biblioteca/services/emprestimo_service.py
+++ gestao_de_biblioteca/services/emprestimo_service.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QMessageBox
+from datetime import datetime
 from infra.repository.emprestimo_repository import EmprestimoRepository
 from infra.repository.usuario_repository import UsuarioRepository
 from infra.repository.livro_repository import LivroRepository
@@ -23,7 +24,7 @@ class EmprestimoService:
         if main_window.tb_acervo_emprestimos.item(selected_row, 0).text() == 'Emprestado':
             QMessageBox.warning(main_window, 'Livro', 'Livro indisponível')
             return
-        livro_emprestimo = self.livro_repository.select_livro_by_titulo(
+        livro_emprestimo = self.livro_repository.select_livro_emprestimo(
             main_window.tb_acervo_emprestimos.item(selected_row, 1).text())
         return livro_emprestimo
 
@@ -51,8 +52,33 @@ class EmprestimoService:
             return
         livro_devolucao = main_window.tb_acervo_emprestimos.item(selected_row, 1).text()
         emprestimo_devolucao = self.emprestimo_repository.select_emprestimo_by_livro(livro_devolucao)
+        dias_atraso = max((datetime.now() - emprestimo_devolucao.data_devolucao).days, 0)
+
         try:
             self.emprestimo_repository.finalize_emprestimo(emprestimo_devolucao)
-            QMessageBox.information(main_window, "Devolução", "Devolução efetuada com sucesso!")
+            QMessageBox.information(main_window, "Devolução", f"Devolução efetuada com sucesso! Dias de atraso: {dias_atraso}")
         except Exception as e:
-            QMessageBox.warning(main_window, "Atenção", f"Erro ao efetuar devolução!\n{e}")
+            QMessageBox.warning(main_window, "Atenção", "Erro ao efetuar devolução!")
+
+    def select_usuario(self, emprestimo_ui):
+        if emprestimo_ui.btn_consultar_emprestimo.text() == 'Limpar':
+            emprestimo_ui.txt_nome_emprestimo.clear()
+            emprestimo_ui.txt_cpf_emprestimo.clear()
+            emprestimo_ui.selected_usuario = None
+            emprestimo_ui.btn_consultar_emprestimo.setText('Consultar')
+        else:
+            try:
+                usuario_emprestimo = self.usuario_repository.select_usuario_by_cpf(emprestimo_ui.
+                                                                                   txt_cpf_emprestimo.
+                                                                                   text())
+                if emprestimo_ui.txt_cpf_emprestimo.text() != '':
+                    emprestimo_ui.txt_nome_emprestimo.setText(usuario_emprestimo.nome)
+                    emprestimo_ui.selected_usuario = usuario_emprestimo
+                    emprestimo_ui.txt_nome_emprestimo.setReadOnly(True)
+                    emprestimo_ui.btn_consultar_emprestimo.setText('Limpar')
+                else:
+                    QMessageBox.warning(emprestimo_ui, "Atenção", "Digite um CPF para consulta!")
+            except Exception as e:
+                QMessageBox.warning(emprestimo_ui, "Atenção", "Usuário não encontrado!")
+                emprestimo_ui.txt_nome_emprestimo.clear()
+
